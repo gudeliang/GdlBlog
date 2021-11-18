e260942ea7af9ea945fbf268d96bd3827a628bc0
gudeliang <gudeliang@msok.com>
2021-11-18 16:18:57 +0800

    项目创建

---
+++ src/test/java/gdl/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package gdl.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
