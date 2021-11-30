51b1a1bac158d95c38fd6eff01d7ff3613d63219
gudeliang <gudeliang@msok.com>
2021-11-30 15:08:08 +0800

    创建主页Index

---
+++ src/main/java/gdl/blog/controller/IndexController.java
@@ -0,0 +1,15 @@
+package gdl.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller //标记这是一个controller
+public class IndexController {
+
+    @GetMapping("/Index") //html 文件名称
+    public String Index() {
+        return "Index";
+    }
+
+
+}
